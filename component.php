1ce84ab269924856bba9cb8ded0a8f1e521c07cb
Axel Wehner <aw@68media.de>
2007-09-28 12:45:00 +0000

    - renamed template_css.css to template.css - added tooltip styles

--- component.php
+++ component.php
@@ -16,7 +16,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<jdoc:include type="head" />
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template_css.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/general.css" type="text/css" />
 </head>
 <body class="contentpane">
